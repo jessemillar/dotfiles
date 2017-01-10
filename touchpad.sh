6f46e60570a60c3c84bf78edd4dcd718178ebdac
Jesse Millar <hellojessemillar@gmail.com>
2017-01-10 16:09:12 -0700

    More touchpad tweaks

--- touchpad.sh
+++ touchpad.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Disable tap to click
-synclient MaxTapTime=0
+# Tweak tap to click
+synclient MaxTapTime=300
+
+# Disable edge scrolling
+synclient VertEdgeScroll=0
 
 # Enable horizontal scrolling
 synclient HorizTwoFingerScroll=1
@@ -10,3 +13,7 @@ synclient HorizTwoFingerScroll=1
 # Higher values equal slower scrolling
 synclient VertScrollDelta=-300
 synclient HorizScrollDelta=-400
+
+# Decrease touchpad sensitivity
+synclient FingerHigh=55
+synclient FingerLow=50
