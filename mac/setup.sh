968475a214ae7efb6c9d3ade48fd4f306066b1d1
Jesse Millar <hellojessemillar@gmail.com>
2017-01-13 09:18:08 -0700

    A lot of organization

---
+++ mac/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f ~/.aliasrc ]; then rm ~/.aliasrc; fi
+ln -s ~/.dotfiles/mac/.aliasrc ~/.aliasrc
+
+if [ -f ~/.dockerrc ]; then rm ~/.dockerrc; fi
+ln -s ~/.dotfiles/mac/.dockerrc ~/.dockerrc
+
+if [ -f ~/.hyper.js ]; then rm ~/.hyper.js; fi
+ln -s ~/.dotfiles/mac/.hyper.js ~/.hyper.js
+
+if [ -f ~/.purerc ]; then rm ~/.purerc; fi
+ln -s ~/.dotfiles/mac/.purerc ~/.purerc
+
+if [ -f ~/.vimrc ]; then rm ~/.vimrc; fi
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+if [ -f ~/.zshrc ]; then rm ~/.zshrc; fi
+ln -s ~/.dotfiles/mac/.zshrc ~/.zshrc
