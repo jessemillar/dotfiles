fb2651d43ab47c89813907c23ce9811b28c7ecfb
Jesse Millar <hellojessemillar@gmail.com>
2017-01-18 07:07:27 -0700

    Tweaking the Linux setup script

--- linux/setup.sh
+++ linux/setup.sh
@@ -9,5 +9,8 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 if [ -d ~/.config/i3 ]; then rm -rf ~/.config/i3; fi
 ln -s ~/.dotfiles/linux/.config/i3 ~/.config/i3
 
+if [ -f ~/.config/gtk-3.0/gtk.css ]; then rm ~/.config/gtk-3.0/gtk.css; fi
+ln -s ~/.dotfiles/linux/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+
 if [ -d ~/.config/polybar ]; then rm -rf ~/.config/polybar; fi
 ln -s ~/.dotfiles/linux/.config/polybar ~/.config/polybar
