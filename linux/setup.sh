5978193a1d3aa3ff13f674be88deabd25cd28085
Jesse Millar <hellojessemillar@gmail.com>
2017-01-19 18:43:36 -0700

    Fixing a git issue

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-if [ -f ~/.aliasrc ]; then rm ~/.aliasrc; fi
 ln -s ~/.dotfiles/linux/.aliasrc ~/.aliasrc
-
-if [ -f ~/.vimrc ]; then rm ~/.vimrc; fi
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-
-if [ -f ~/.Xdefaults ]; then rm ~/.Xdefaults; fi
 ln -s ~/.dotfiles/linux/.Xdefaults ~/.Xdefaults
-
-if [ -d ~/.config/i3 ]; then rm -rf ~/.config/i3; fi
 ln -s ~/.dotfiles/linux/.config/i3 ~/.config/i3
-
-if [ -f ~/.config/gtk-3.0/gtk.css ]; then rm ~/.config/gtk-3.0/gtk.css; fi
+ln -s ~/.dotfiles/linux/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/linux/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
-
-if [ -f ~/.purerc ]; then rm ~/.purerc; fi
 ln -s ~/.dotfiles/.purerc ~/.purerc
-
-if [ -d ~/.config/polybar ]; then rm -rf ~/.config/polybar; fi
 ln -s ~/.dotfiles/linux/.config/polybar ~/.config/polybar
-
-if [ -f ~/.zshrc ]; then rm ~/.zshrc; fi
 ln -s ~/.dotfiles/linux/.zshrc ~/.zshrc
--- mac/setup.sh
+++ mac/setup.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-if [ -f ~/.aliasrc ]; then rm ~/.aliasrc; fi
 ln -s ~/.dotfiles/mac/.aliasrc ~/.aliasrc
-
-if [ -f ~/.dockerrc ]; then rm ~/.dockerrc; fi
 ln -s ~/.dotfiles/mac/.dockerrc ~/.dockerrc
-
-if [ -f ~/.hyper.js ]; then rm ~/.hyper.js; fi
 ln -s ~/.dotfiles/mac/.hyper.js ~/.hyper.js
-
-if [ -f ~/.config/nvim/init.vim ]; then rm ~/.config/nvim/init.vim; fi
 ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
-
-if [ -f ~/.purerc ]; then rm ~/.purerc; fi
 ln -s ~/.dotfiles/.purerc ~/.purerc
-
-if [ -f ~/.vimrc ]; then rm ~/.vimrc; fi
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-
-if [ -f ~/.zshrc ]; then rm ~/.zshrc; fi
 ln -s ~/.dotfiles/mac/.zshrc ~/.zshrc
