d59a3812bac8c186f343eb211b2d59f8b16dbcb3
Jesse Millar <hellojessemillar@gmail.com>
2017-02-05 10:24:05 -0700

    Getting some things tweaked in my new Arch install

--- linux/personal/setup.sh
+++ linux/personal/setup.sh
@@ -9,18 +9,8 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 rm ~/.Xdefaults
 ln -s ~/.dotfiles/linux/.Xdefaults ~/.Xdefaults
 
-rm -rf ~/.config/i3
-ln -s ~/.dotfiles/linux/personal/.config/i3 ~/.config/i3
-
 rm ~/.gitconfig
 ln -s ~/.dotfiles/linux/.gitconfig ~/.gitconfig
-
-rm ~/.config/gtk-3.0/gtk.css
-mkdir -p ~/.config/gtk-3.0
-ln -s ~/.dotfiles/linux/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
-
-rm -rf ~/.config/polybar
-ln -s ~/.dotfiles/linux/personal/.config/polybar ~/.config/polybar
 
 rm ~/.zshrc
 ln -s ~/.dotfiles/linux/personal/.zshrc ~/.zshrc
@@ -33,3 +23,12 @@ ln -s ~/.dotfiles/.vim/colors ~/.vim/colors
 
 rm ~/.functionsrc
 ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
+
+rm ~/.compton.conf
+ln -s ~/.dotfiles/linux/.compton.conf ~/.compton.conf
+
+rm -rf ~/.config/bspwm
+ln -s ~/.dotfiles/linux/personal/.config/bspwm
+
+rm -rf ~/.config/sxhkd
+ln -s ~/.dotfiles/linux/personal/.config/sxhkd
