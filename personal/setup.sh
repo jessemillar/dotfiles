988b1254e2c77459307ed4b8843a6b66cecf3164
Jesse Millar <hellojessemillar@gmail.com>
2017-02-13 09:45:31 -0700

    Removing Mac dotfiles because I've moved on

--- personal/setup.sh
+++ personal/setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 rm ~/.aliasrc
-ln -s ~/.dotfiles/linux/personal/.aliasrc ~/.aliasrc
+ln -s ~/.dotfiles/personal/.aliasrc ~/.aliasrc
 
 rm ~/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 rm ~/.Xdefaults
-ln -s ~/.dotfiles/linux/.Xdefaults ~/.Xdefaults
+ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
 
 rm ~/.gitconfig
-ln -s ~/.dotfiles/linux/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 rm ~/.zshrc
-ln -s ~/.dotfiles/linux/personal/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/personal/.zshrc ~/.zshrc
 
 rm ~/.oh-my-zsh/custom/mister-michael.zsh-theme
 ln -s ~/.dotfiles/.oh-my-zsh/custom/mister-michael.zsh-theme ~/.oh-my-zsh/custom/mister-michael.zsh-theme
@@ -25,13 +25,13 @@ rm ~/.functionsrc
 ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
 
 rm -rf ~/.config/bspwm
-ln -s ~/.dotfiles/linux/personal/.config/bspwm ~/.config
+ln -s ~/.dotfiles/personal/.config/bspwm ~/.config
 
 rm -rf ~/.config/sxhkd
-ln -s ~/.dotfiles/linux/personal/.config/sxhkd ~/.config
+ln -s ~/.dotfiles/personal/.config/sxhkd ~/.config
 
 sudo rm /etc/X11/xorg.conf.d/10-mtrack.conf
-sudo ln -s ~/.dotfiles/linux/personal/etc/X11/xorg.conf.d/10-mtrack.conf /etc/X11/xorg.conf.d/10-mtrack.conf
+sudo ln -s ~/.dotfiles/personal/etc/X11/xorg.conf.d/10-mtrack.conf /etc/X11/xorg.conf.d/10-mtrack.conf
 
 sudo rm /etc/systemd/system/sedate.service
-sudo ln -s ~/.dotfiles/linux/personal/etc/systemd/system/sedate.service /etc/systemd/system/sedate.service
+sudo ln -s ~/.dotfiles/personal/etc/systemd/system/sedate.service /etc/systemd/system/sedate.service
--- work/setup.sh
+++ work/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 rm ~/.aliasrc
-ln -s ~/.dotfiles/linux/work/.aliasrc ~/.aliasrc
+ln -s ~/.dotfiles/work/.aliasrc ~/.aliasrc
 
 rm ~/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 
 rm ~/.Xdefaults
-ln -s ~/.dotfiles/linux/.Xdefaults ~/.Xdefaults
+ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
 
 rm -rf ~/.config/i3
-ln -s ~/.dotfiles/linux/work/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/work/.config/i3 ~/.config/i3
 
 rm ~/.gitconfig
-ln -s ~/.dotfiles/linux/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 rm -rf ~/.config/polybar
-ln -s ~/.dotfiles/linux/work/.config/polybar ~/.config/polybar
+ln -s ~/.dotfiles/work/.config/polybar ~/.config/polybar
 
 rm ~/.zshrc
-ln -s ~/.dotfiles/linux/work/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/work/.zshrc ~/.zshrc
 
 rm ~/.oh-my-zsh/custom/mister-michael.zsh-theme
 ln -s ~/.dotfiles/.oh-my-zsh/custom/mister-michael.zsh-theme ~/.oh-my-zsh/custom/mister-michael.zsh-theme
@@ -31,10 +31,10 @@ rm ~/.functionsrc
 ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
 
 rm -rf ~/.config/bspwm
-ln -s ~/.dotfiles/linux/work/.config/bspwm ~/.config
+ln -s ~/.dotfiles/work/.config/bspwm ~/.config
 
 rm -rf ~/.config/sxhkd
-ln -s ~/.dotfiles/linux/work/.config/sxhkd ~/.config
+ln -s ~/.dotfiles/work/.config/sxhkd ~/.config
 
 sudo rm /etc/X11/xorg.conf.d/10-mouse.conf
-sudo ln -s ~/.dotfiles/linux/personal/etc/X11/xorg.conf.d/10-mouse.conf /etc/X11/xorg.conf.d/10-mouse.conf
+sudo ln -s ~/.dotfiles/personal/etc/X11/xorg.conf.d/10-mouse.conf /etc/X11/xorg.conf.d/10-mouse.conf
