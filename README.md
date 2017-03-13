8a08c95b1e649c86587df2041c401f31b7e3e9eb
Jesse Millar <hellojessemillar@gmail.com>
2017-03-13 10:37:34 -0600

    Further cleanup

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## Setup
 See [the wiki](https://github.com/jessemillar/dotfiles/wiki) for progam setup. Use one of the following commands to configure dotfiles.
 ```
-./lapbop/setup.sh
+./setup.sh
 ```
 
+My laptop needs a bit of custom setup so I run this script on it:
 ```
-./dual-monitor/setup.sh
+./laptop/setup.sh
 ```
--- laptop/setup.sh
+++ laptop/setup.sh
@@ -1,40 +1,7 @@
 #!/bin/bash
 
-rm ~/.aliasrc
-ln -s ~/.dotfiles/.aliasrc ~/.aliasrc
-
-rm ~/.vimrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-
-rm ~/.Xdefaults
-ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
-
-rm ~/.gitconfig
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
-rm ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-
-rm ~/.oh-my-zsh/custom/mister-michael.zsh-theme
-ln -s ~/.dotfiles/.oh-my-zsh/custom/mister-michael.zsh-theme ~/.oh-my-zsh/custom/mister-michael.zsh-theme
-
-rm -rf ~/.vim/colors
-ln -s ~/.dotfiles/.vim/colors ~/.vim/colors
-
-rm ~/.functionsrc
-ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
-
-rm -rf ~/.config/bspwm
-ln -s ~/.dotfiles/.config/bspwm ~/.config
-
-rm -rf ~/.config/sxhkd
-ln -s ~/.dotfiles/.config/sxhkd ~/.config
-
 sudo rm /etc/X11/xorg.conf.d/10-mtrack.conf
 sudo ln -s ~/.dotfiles/etc/X11/xorg.conf.d/10-mtrack.conf /etc/X11/xorg.conf.d/10-mtrack.conf
-
-sudo rm /etc/X11/xorg.conf.d/10-mouse.conf
-sudo ln -s ~/.dotfiles/etc/X11/xorg.conf.d/10-mouse.conf /etc/X11/xorg.conf.d/10-mouse.conf
 
 sudo rm /etc/systemd/system/sedate.service
 sudo ln -s ~/.dotfiles/etc/systemd/system/sedate.service /etc/systemd/system/sedate.service
