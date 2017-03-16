2ef118c36f5e02b70959063d78cdcc1b461466ad
Jesse Millar <hellojessemillar@gmail.com>
2017-03-16 14:02:32 -0600

    Fixing the battery timer and changing trackpad drivers

--- laptop/setup.sh
+++ laptop/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo rm /etc/X11/xorg.conf.d/10-mtrack.conf
-sudo ln -s ~/.dotfiles/etc/X11/xorg.conf.d/10-mtrack.conf /etc/X11/xorg.conf.d/10-mtrack.conf
+sudo rm /etc/X11/xorg.conf.d/10-trackpad.conf
+sudo ln -s ~/.dotfiles/etc/X11/xorg.conf.d/10-trackpad.conf /etc/X11/xorg.conf.d/10-trackpad.conf
 
 sudo rm /etc/systemd/system/sedate.service
 sudo ln -s ~/.dotfiles/etc/systemd/system/sedate.service /etc/systemd/system/sedate.service
