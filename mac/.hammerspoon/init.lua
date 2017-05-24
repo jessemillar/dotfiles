aac930726d78ecec2c6614e976154e65b7163f2e
Jesse Millar <hellojessemillar@gmail.com>
2017-05-24 14:49:27 -0500

    Adding keyboard shortcuts

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -1,11 +1,57 @@
--- open a terminal like in bspwm
+-- open a terminal
 hs.hotkey.bind({"ctrl"}, "return", function()
-  hs.execute("open -n -a Hyper")
+  hs.execute("open -na Hyper")
+end)
+
+-- open slack
+hs.hotkey.bind({"ctrl"}, "S", function()
+  hs.execute("open -na Slack")
+end)
+
+-- open spotify
+hs.hotkey.bind({"ctrl"}, "P", function()
+  hs.execute("open -na Spotify")
+end)
+
+-- open google chrome
+hs.hotkey.bind({"ctrl"}, "C", function()
+  hs.execute("open -na 'Google Chrome'")
+end)
+
+-- open verizon texting in a new window
+hs.hotkey.bind({"ctrl", "shift"}, "V", function()
+  hs.execute("open -na 'Google Chrome' --args --new-window 'https://web.vma.vzw.com/vma/webs2/Message.do'")
+end)
+
+-- open verizon texting
+hs.hotkey.bind({"ctrl"}, "V", function()
+  hs.execute("open -a 'Google Chrome' 'https://web.vma.vzw.com/vma/webs2/Message.do'")
+end)
+
+-- open trello in a new window
+hs.hotkey.bind({"ctrl", "shift"}, "R", function()
+  hs.execute("open -na 'Google Chrome' --args --new-window 'https://trello.com'")
+end)
+
+-- open trello
+hs.hotkey.bind({"ctrl"}, "R", function()
+  hs.execute("open -a 'Google Chrome' 'https://trello.com'")
+end)
+
+-- open google inbox in a new window
+hs.hotkey.bind({"ctrl", "shift"}, "E", function()
+  hs.execute("open -na 'Google Chrome' --args --new-window 'https://inbox.google.com'")
+end)
+
+-- open google inbox
+hs.hotkey.bind({"ctrl"}, "E", function()
+  hs.execute("open -a 'Google Chrome' 'https://inbox.google.com'")
 end)
 
 -- defeat paste blocking on websites
 hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
+-- reload config files
 function reloadConfig(files)
     doReload = false
     for _,file in pairs(files) do
@@ -18,5 +64,5 @@ function reloadConfig(files)
     end
 end
 
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+-- watch for changes to the config files
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
