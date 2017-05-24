390cadb9195c4cd9a78c17df0330cde445f45529
Jesse Millar <hellojessemillar@gmail.com>
2017-05-24 15:39:42 -0500

    Moving to Option as my keyboard shortcut super key

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -1,55 +1,52 @@
 -- open a terminal
-hs.hotkey.bind({"ctrl"}, "return", function()
+hs.hotkey.bind({"alt"}, "return", function()
   hs.execute("open -na Hyper")
 end)
 
 -- open slack
-hs.hotkey.bind({"ctrl"}, "S", function()
+hs.hotkey.bind({"alt"}, "S", function()
   hs.execute("open -na Slack")
 end)
 
 -- open spotify
-hs.hotkey.bind({"ctrl"}, "P", function()
+hs.hotkey.bind({"alt"}, "P", function()
   hs.execute("open -na Spotify")
 end)
 
 -- open google chrome
-hs.hotkey.bind({"ctrl"}, "C", function()
+hs.hotkey.bind({"alt"}, "C", function()
   hs.execute("open -na 'Google Chrome'")
 end)
 
 -- open verizon texting in a new window
-hs.hotkey.bind({"ctrl", "shift"}, "V", function()
+hs.hotkey.bind({"alt", "shift"}, "V", function()
   hs.execute("open -na 'Google Chrome' --args --new-window 'https://web.vma.vzw.com/vma/webs2/Message.do'")
 end)
 
 -- open verizon texting
-hs.hotkey.bind({"ctrl"}, "V", function()
+hs.hotkey.bind({"alt"}, "V", function()
   hs.execute("open -a 'Google Chrome' 'https://web.vma.vzw.com/vma/webs2/Message.do'")
 end)
 
 -- open trello in a new window
-hs.hotkey.bind({"ctrl", "shift"}, "R", function()
+hs.hotkey.bind({"alt", "shift"}, "R", function()
   hs.execute("open -na 'Google Chrome' --args --new-window 'https://trello.com'")
 end)
 
 -- open trello
-hs.hotkey.bind({"ctrl"}, "R", function()
+hs.hotkey.bind({"alt"}, "R", function()
   hs.execute("open -a 'Google Chrome' 'https://trello.com'")
 end)
 
 -- open google inbox in a new window
-hs.hotkey.bind({"ctrl", "shift"}, "E", function()
+hs.hotkey.bind({"alt", "shift"}, "E", function()
   hs.execute("open -na 'Google Chrome' --args --new-window 'https://inbox.google.com'")
 end)
 
 -- open google inbox
-hs.hotkey.bind({"ctrl"}, "E", function()
+hs.hotkey.bind({"alt"}, "E", function()
   hs.execute("open -a 'Google Chrome' 'https://inbox.google.com'")
 end)
-
--- defeat paste blocking on websites
-hs.hotkey.bind({"cmd", "alt"}, "V", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 
 -- reload config files
 function reloadConfig(files)
