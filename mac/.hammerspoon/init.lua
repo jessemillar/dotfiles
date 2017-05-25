48859d2266f2f9c25c5141fdf238b89dd5ca176c
Jesse Millar <hellojessemillar@gmail.com>
2017-05-25 14:26:31 -0500

    Fixing terminal command cancellation and enabling automatic work proxy configuration

--- mac/.hammerspoon/init.lua
+++ mac/.hammerspoon/init.lua
@@ -1,65 +1,70 @@
 -- open a terminal
-hs.hotkey.bind({"alt"}, "return", function()
-  hs.execute("open -na Hyper")
+hs.hotkey.bind({"ctrl"}, "return", function()
+	hs.execute("open -na Hyper")
 end)
 
 -- open slack
-hs.hotkey.bind({"alt"}, "S", function()
-  hs.execute("open -na Slack")
+hs.hotkey.bind({"ctrl", "shift"}, "S", function()
+	hs.execute("open -na Slack")
 end)
 
 -- open spotify
-hs.hotkey.bind({"alt"}, "P", function()
-  hs.execute("open -na Spotify")
+hs.hotkey.bind({"ctrl", "shift"}, "P", function()
+	hs.execute("open -na Spotify")
 end)
 
 -- open google chrome
-hs.hotkey.bind({"alt"}, "C", function()
-  hs.execute("open -na 'Google Chrome'")
+hs.hotkey.bind({"ctrl", "shift"}, "C", function()
+	hs.execute("open -na 'Google Chrome'")
 end)
 
 -- open verizon texting in a new window
-hs.hotkey.bind({"alt", "shift"}, "V", function()
-  hs.execute("open -na 'Google Chrome' --args --new-window 'https://web.vma.vzw.com/vma/webs2/Message.do'")
+hs.hotkey.bind({"ctrl", "shift"}, "V", function()
+	hs.execute("open -na 'Google Chrome' --args --new-window 'https://web.vma.vzw.com/vma/webs2/Message.do'")
 end)
 
 -- open verizon texting
-hs.hotkey.bind({"alt"}, "V", function()
-  hs.execute("open -a 'Google Chrome' 'https://web.vma.vzw.com/vma/webs2/Message.do'")
-end)
-
--- open trello in a new window
-hs.hotkey.bind({"alt", "shift"}, "R", function()
-  hs.execute("open -na 'Google Chrome' --args --new-window 'https://trello.com'")
-end)
-
--- open trello
-hs.hotkey.bind({"alt"}, "R", function()
-  hs.execute("open -a 'Google Chrome' 'https://trello.com'")
-end)
-
--- open google inbox in a new window
-hs.hotkey.bind({"alt", "shift"}, "E", function()
-  hs.execute("open -na 'Google Chrome' --args --new-window 'https://inbox.google.com'")
-end)
-
--- open google inbox
-hs.hotkey.bind({"alt"}, "E", function()
-  hs.execute("open -a 'Google Chrome' 'https://inbox.google.com'")
+hs.hotkey.bind({"ctrl"}, "V", function()
+	hs.execute("open -a 'Google Chrome' 'https://web.vma.vzw.com/vma/webs2/Message.do'")
 end)
 
 -- reload config files
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
+	doReload = false
+	for _,file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
 end
 
 -- watch for changes to the config files
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+-- watch for work wifi
+wifiWatcher = nil
+workSSID = "Eagle"
+lastSSID = hs.wifi.currentNetwork()
+
+function ssidChangedCallback()
+	network = hs.network.configuration.open()
+	newSSID = hs.wifi.currentNetwork()
+
+	if newSSID == workSSID and lastSSID ~= workSSID then
+		-- we just joined work wifi
+		hs.execute("mv ~/.envrcbak ~/.envrc")
+		network:setLocation("Work")
+	elseif newSSID ~= workSSID and lastSSID == workSSID then
+		-- we just left work wifi
+		hs.execute("mv ~/.envrc ~/.envrcbak")
+		network:setLocation("Home/Other")
+	end
+
+	lastSSID = newSSID
+end
+
+wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
+wifiWatcher:start()
