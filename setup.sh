3ca4644d15afe24397b51879080f231ef1aa5471
Jesse Millar <hellojessemillar@gmail.com>
2018-01-17 09:02:50 -0600

    Adding my wd config

--- setup.sh
+++ setup.sh
@@ -3,26 +3,8 @@
 rm ~/.aliasrc
 ln -s ~/.dotfiles/.aliasrc ~/.aliasrc
 
-rm ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-
-rm ~/.hyper.js
-ln -s ~/.dotfiles/.hyper.js ~/.hyper.js
-
-rm -rf ~/.hammerspoon
-ln -s ~/.dotfiles/.hammerspoon ~/.hammerspoon
-
-rm ~/.functionsrc
-ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
-
-rm -rf ~/.khdrc
-ln -s ~/.dotfiles/.khdrc ~/.khdrc
-
 rm ~/.chunkwmrc
 ln -s ~/.dotfiles/.chunkwmrc ~/.chunkwmrc
-
-rm ~/.ideavimrc
-ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
 
 rm -rf ~/.config/karabiner
 ln -s ~/.dotfiles/.config/karabiner ~/.config/karabiner
@@ -30,10 +12,32 @@ ln -s ~/.dotfiles/.config/karabiner ~/.config/karabiner
 rm -rf ~/.config/nvim
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
+rm ~/.functionsrc
+ln -s ~/.dotfiles/.functionsrc ~/.functionsrc
+
 rm ~/.gitignore_global
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
+rm -rf ~/.hammerspoon
+ln -s ~/.dotfiles/.hammerspoon ~/.hammerspoon
+
+rm ~/.hyper.js
+ln -s ~/.dotfiles/.hyper.js ~/.hyper.js
+
+rm ~/.ideavimrc
+ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
+
+rm -rf ~/.khdrc
+ln -s ~/.dotfiles/.khdrc ~/.khdrc
+
 rm ~/.oh-my-zsh/custom/mister-michael.zsh-theme
 mkdir -p ~/.oh-my-zsh/custom
 ln -s ~/.dotfiles/.oh-my-zsh/custom/mister-michael.zsh-theme ~/.oh-my-zsh/custom/mister-michael.zsh-theme
+
+rm ~/.warprc
+ln -s ~/.dotfiles/.warprc ~/.warprc
+
+rm ~/.zshrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
