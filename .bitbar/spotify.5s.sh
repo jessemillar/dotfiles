c2c8d933ff6575dc9e69a5a666e6a4232e5d88b6
Jesse Millar <hellojessemillar@gmail.com>
2019-03-06 14:53:00 -0600

    More simplification and giving some attribution

--- .bitbar/spotify.5s.sh
+++ .bitbar/spotify.5s.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-# Send a series of semicolon-delimited commands to Spotify
-function tellspotify() {
-  commands="$(echo "$1" | tr ";" "\\n")"
+# This is a simplification of https://github.com/matryer/bitbar-plugins/blob/master/Music/spotify.10s.sh
+icon="♪"
 
+# Send a command to Spotify
+function tellspotify() {
   osascript -e "
             tell application \"Spotify\"
-                $commands
+                $1
             end tell";
 }
 
+# Show just an icon if Spotify isn't running
 if [ "$(osascript -e 'application "Spotify" is running')" = "false" ]; then
-  echo "♪"
+  echo $icon
   echo "---"
   echo "Spotify is not running"
   exit
 fi
 
 ## Get Spotify info
-
-icon="♪"
 state=$(tellspotify 'player state as string');
 track=$(tellspotify 'name of current track as string');
 artist=$(tellspotify 'artist of current track as string');
 album=$(tellspotify 'album of current track as string');
 
 ## Print the display
-
 if [ "$state" = "playing" ]; then
 	echo "$track - $artist $icon"
 else
   echo "♪"
 fi
 
+# Print submenu items
 echo "---"
-
 echo -e "Track:\\t$track"
 echo -e "Artist:\\t$artist"
 echo -e "Album:\\t$album"
