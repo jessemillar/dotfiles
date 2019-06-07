4fd4007215edc8cdf7c99118d1d57fde9d87b199
Jesse Millar <hellojessemillar@gmail.com>
2019-06-07 11:59:25 -0500

    Making wallpapers auto set

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Setup
 See [the wiki](https://github.com/jessemillar/dotfiles/wiki/Mac-Setup) for program setup. Use the following command to configure dotfiles after cloning this repo:
 ```
-./setup.sh
+./update.sh
 ```
--- update.sh
+++ update.sh
@@ -57,3 +57,10 @@ ln -s ~/.dotfiles/.warprc ~/.warprc
 rm ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
+for i in {0..9}
+do
+	chunkc tiling::window -d $i
+	skhd -k "cmd - ${i}"
+	sleep 1
+	wallpaper set ~/Documents/Trunk/wallpaper/randomized
+done
