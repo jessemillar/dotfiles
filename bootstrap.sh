cd498d41fcc0c8cd3081efd81d939eeba9d7dfbb
Jesse Millar <hellojessemillar@gmail.com>
2019-07-25 16:51:00 -0500

    Starting to really organize my dotfiles thanks to GNU Stow

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
 # TODO Use Ansible for installing applications
-# TODO Use GNU Stow to create symlinks
-# TODO Figure out how to keep Ansible updated
+# TODO Maybe use Stow inside Ansible
 
-# Install pip, the Python package manager
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
+# Install the Homebrew package manager
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Ansible via pip
-pip install --user ansible
+# Install Ansible via Homebrew
+brew install ansible
+ansible --version
+
+# Set up dotfile symlinks via GNU Stow
+stow ack
+stow bitbar
+stow git
+stow hammerspoon
+stow intellij-idea
+stow karabiner
+stow kitty
+stow neovim
+stow skhd
+stow visual-studio-code
+stow yabai
+stow zsh
+
+# Print a message on completion
+echo "bootstrap.sh finished"
