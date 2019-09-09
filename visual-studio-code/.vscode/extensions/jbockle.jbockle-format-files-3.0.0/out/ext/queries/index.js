6358a9ccd742c8f21da0724cb99f08fd799265c7
Jesse Millar <hellojessemillar@gmail.com>
2019-09-09 13:16:46 -0500

    LOTS of Visual Studio Code updates

--- visual-studio-code/.vscode/extensions/jbockle.jbockle-format-files-3.0.0/out/ext/queries/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("./config");
-const files_1 = require("./files");
-exports.default = {
-    config: new config_1.Config(),
-    files: new files_1.Files(),
-};
-//# sourceMappingURL=index.js.map
--- visual-studio-code/.vscode/extensions/scalameta.metals-1.5.2/out/client-commands.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Metals expects clients to implement the following commands: https://scalameta.org/metals/docs/editors/new-editor.html#metals-client-commands
-exports.ClientCommands = {
-    toggleLogs: "metals-logs-toggle",
-    focusDiagnostics: "metals-diagnostics-focus",
-    runDoctor: "metals-doctor-run"
-};
-//# sourceMappingURL=client-commands.js.map
