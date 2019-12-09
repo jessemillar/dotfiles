5f8a55705897a3afa75783b90c3810b5e49134e2
Jesse Millar <jesse.millar@microsoft.com>
2019-12-09 14:12:38 -0600

    Handling the ~/.config directory properly
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ansible-playbook ansible-playbook-*.yml
+ansible-playbook ansible-playbook-*.yml -vvvv
 
 # Print a message on completion
 echo -e "windows/bootstrap.sh finished"
