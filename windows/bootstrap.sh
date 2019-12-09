8b5354c16a42636a916b5324ea491d5a9daf5845
Jesse Millar <jesse.millar@microsoft.com>
2019-12-09 15:24:24 -0600

    Adding in all the choco installs I actually use
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install Chocolatey
+cmd.exe /C @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
 ansible-playbook ansible-playbook-*.yml
 
 # Print a message on completion
