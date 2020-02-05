2fcc5faab7c5c2ccd0abc335edcd643abcc73426
Jesse Millar <jesse.millar@microsoft.com>
2020-02-05 09:18:47 -0600

    Adding comments to the Pi script
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- bootstrap-pi.sh
+++ bootstrap-pi.sh
@@ -5,16 +5,26 @@ set -e
 # Enable debug mode to print out commands a la Ansible prior to running them
 set -x
 
+# Make sure the system is up to date
 sudo apt update
 sudo apt upgrade -y
+sudo apt dist-upgrade -y
+
+# Install stow since it's needed for some pre-installation setup
 sudo apt install -y stow
+
+# Make needed directories
 mkdir -p ~/.config
 mkdir -p ~/.bin
+
+# Stow general config files
 stow ack
 stow git
 stow neovim
 stow starship
 stow tmux
+
+# Install general packages
 sudo apt install -y ack fzf git golang grep imagemagick less neovim python python3 python3-pip shellcheck tldr tmux tree unzip watch xfce4 zsh
 
 # Install Rust
@@ -24,23 +34,31 @@ cargo install starship
 
 # Install fzf zsh support
 # $(brew --prefix)/opt/fzf/install --all
+
 # Install neovim Python module
 pip3 install neovim
+
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Install nvim plugins
 nvim --headless +PlugUpdate +PlugUpgrade +UpdateRemotePlugins +GoUpdateBinaries +qall
+
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended || true
+
 # Remove the default ~/.zshrc
 rm ~/.zshrc || true
 stow zsh
+
 # Change user shell to Zsh
 command -v zsh | sudo tee -a /etc/shells && sudo usermod --shell $(command -v zsh) $(whoami)
+
 # Generate SSH keypairs
 cat /dev/zero | ssh-keygen -q -N "" || true
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
 # Install imcat
 git clone https://github.com/stolk/imcat.git && cd imcat && make && mv imcat ~/.bin && cd .. && rm -rf imcat
 
