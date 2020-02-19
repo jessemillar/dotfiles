51ddbb12ed108f20fcd09fbfa0032c010ae53200
Jesse Millar <jesse.millar@microsoft.com>
2020-02-19 15:21:52 -0600

    Making Ansible update Homebrew stuff
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
+function ruler {
+	printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+	echo -e "$1"
+	printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
 # Install the Homebrew package manager
 case $(uname -s) in
 Darwin*)
 	# Mac
+	ruler "Installing Homebrew for macOS"
   yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   ;;
 *)
 	# Linux (WSL included)
+	ruler "Updating packages via apt"
 	sudo apt update
-	sudo apt upgrade -y
+	sudo apt full-upgrade -y
+	ruler "Installing Linuxbrew dependencies via apt"
 	sudo apt install -y build-essential curl file git
+	ruler "Installing Linuxbrew"
 	yes "" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 	# Put Homebrew in the PATH until the script can do it in a more permanent way
-	eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   ;;
 esac
 
 # Install Ansible via Homebrew
+ruler "Installing ansible with brew"
 brew install ansible
+brew upgrade ansible
 
 # Run the playbooks
+ruler "Running ansible-playbook-main.yml"
 ansible-playbook ansible-playbook-main.yml
 
 # Print a message on completion
-echo -e "bootstrap.sh finished\nRun any os-specific bootstrap.sh scripts and then reboot"
+ruler "bootstrap.sh finished; run any os-specific bootstrap.sh scripts and then reboot"
