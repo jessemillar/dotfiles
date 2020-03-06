af25b422f840d3661766369428efcfab37cdf785
Jesse Millar <jesse.millar@microsoft.com>
2020-03-06 09:58:54 -0600

    Cloning dotfiles into Azure with HTTPS
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- azure/bootstrap.sh
+++ azure/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:jessemillar/dotfiles.git
+git clone https://github.com/jessemillar/dotfiles.git || exit
 mv dotfiles .dotfiles
 cd .dotfiles || exit
 ./bootstrap.sh
