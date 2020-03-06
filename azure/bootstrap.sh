4028a73d81eb866819c47160799fe8ff4064332d
Jesse Millar <jesse.millar@microsoft.com>
2020-03-06 18:30:50 +0000

    Setting the dotfiles remote URL to SSH after finishing Azure bootstrapping
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- azure/bootstrap.sh
+++ azure/bootstrap.sh
@@ -4,3 +4,4 @@ git clone https://github.com/jessemillar/dotfiles.git || exit
 mv dotfiles .dotfiles
 cd .dotfiles || exit
 ./bootstrap.sh
+git remote set-url origin git@github.com:jessemillar/dotfiles.git
