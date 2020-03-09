49bb3f29dc16185d60bbb1db1cbba4e63f83ad45
Jesse Millar <jesse.millar@microsoft.com>
2020-03-08 19:55:14 -0500

    Cleaning up the Aseprite compilation script
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- raspberry-pi/aseprite/compile.sh
+++ raspberry-pi/aseprite/compile.sh
@@ -3,42 +3,48 @@
 # See https://github.com/aseprite/aseprite/issues/1735
 
 # Install dependencies
-sudo apt install -y g++ cmake ninja-build libx11-dev libxcursor-dev libgl1-mesa-dev libfontconfig1-dev
-sudo apt install clang
+sudo apt update
+sudo apt install -y g++ cmake ninja-build libx11-dev libxcursor-dev libgl1-mesa-dev libfontconfig1-dev clang
+
+# Create the directory we'll clone dependencies into
+mkdir "$HOME"/.aseprite-deps
+cd "$HOME"/.aseprite-deps || exit
 
 # Install gn
 git clone https://gn.googlesource.com/gn
 cd gn || exit
 python build/gen.py
 ninja -C out
+cd ..
 
 # Install Skia library
-mkdir $HOME/.deps
-cd $HOME/.deps
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 git clone -b aseprite-m81 https://github.com/aseprite/skia.git
 export PATH="${PATH}:${PWD}/depot_tools"
-cd skia
+cd skia || exit
 python tools/git-sync-deps
 gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false"
 ninja -C out/Release-x64 skia modules
+cd ..
 
+# Clone the Aseprite repo
 git clone --recursive https://github.com/aseprite/aseprite.git
 
 # Update existing pull
-cd aseprite
-git pull
-git submodule update --init --recursive
+# TODO Make the script smart enough to update
+# cd aseprite
+# git pull
+# git submodule update --init --recursive
 
-cd aseprite
+cd aseprite || exit
 mkdir build
-cd build
+cd build || exit
 cmake \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DLAF_BACKEND=skia \
-  -DSKIA_DIR=$HOME/deps/skia \
+  -DSKIA_DIR="$HOME"/deps/skia \
 	-DUSE_SHARED_CURL=yes \
-  -DSKIA_LIBRARY_DIR=$HOME/deps/skia/out/Release-x64 \
+  -DSKIA_LIBRARY_DIR="$HOME"/deps/skia/out/Release-x64 \
   -G Ninja \
   ..
 ninja aseprite
