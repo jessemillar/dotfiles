b702910a9d0e7c1f108bfcd0e6f52de4f8919ab6
Jesse Millar <jesse.millar@microsoft.com>
2020-03-10 13:05:59 -0500

    Don't need to make ~/projects in bootstrap.sh
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- azure/bootstrap.sh
+++ azure/bootstrap.sh
@@ -2,7 +2,6 @@
 
 git clone https://github.com/jessemillar/dotfiles.git || exit
 mv dotfiles .dotfiles
-mkdir projects || true
 cd .dotfiles || exit
 ./bootstrap.sh
 git remote set-url origin git@github.com:jessemillar/dotfiles.git
