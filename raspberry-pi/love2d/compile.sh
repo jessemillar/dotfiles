db3f7b393a770585aecf93e45125a04821511ee5
Jesse Millar <jesse.millar@microsoft.com>
2020-03-18 12:10:21 -0500

    A little more verbosity in the Love compilation script
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- raspberry-pi/love2d/compile.sh
+++ raspberry-pi/love2d/compile.sh
@@ -11,16 +11,18 @@ ruler "Checking for existing LÖVE"
 if ! [ -x "$(command -v love)" ]
 then
 	ruler "Installing dependencies"
-	sudo apt-get install -y build-essential autotools-dev automake libtool pkg-config libfreetype6-dev libluajit-5.1-dev libphysfs-dev libsdl2-dev libopenal-dev libogg-dev libvorbis-dev libmodplug-dev libmpg123-dev libtheora-dev
+	sudo apt install -y build-essential autotools-dev automake libtool pkg-config libfreetype6-dev libluajit-5.1-dev libphysfs-dev libsdl2-dev libopenal-dev libogg-dev libvorbis-dev libmodplug-dev libmpg123-dev libtheora-dev
 	cd
 	ruler "Cloning repo"
 	git clone https://github.com/love2d/love
 	mv love .love
 	cd .love
+	ruler "Compiling version 11.3"
 	git checkout 11.3
 	./platform/unix/automagic
 	./configure
 	make
+	ruler "Symlinking executable to ~/.bin"
 	ln -s ~/.love/src/love ~/.bin
 fi
 
