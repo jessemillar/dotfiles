9c73ba1325de25180296c14c6f7dd9e8e71fb7be
Jesse Millar <jesse.millar@microsoft.com>
2022-05-17 14:01:50 -0500

    Installing python3 instead and in the Ansible bootstrap file

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Ansible via Homebrew
-reverb "Installing ansible with brew"
+reverb "Installing Ansible dependencies via apt-get"
+sudo apt-get install -y python3
+reverb "Installing Ansible with brew"
 brew install ansible
