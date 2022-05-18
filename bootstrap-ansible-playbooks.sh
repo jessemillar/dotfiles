65c997d6dc5c9b84f06ed52773d8867b2ab959f6
Jesse Millar <jesse.millar@microsoft.com>
2022-05-18 11:57:18 -0500

    Disable Ansible verbose debugging

--- bootstrap-ansible-playbooks.sh
+++ bootstrap-ansible-playbooks.sh
@@ -3,5 +3,5 @@
 # Run the playbooks with debug printing
 reverb "Running ansible-playbook-main.yml"
 export ANSIBLE_LOG_PATH=~/ansible.log
-export ANSIBLE_DEBUG=True
+#export ANSIBLE_DEBUG=True
 ansible-playbook -vvv ansible-playbook-main.yml
