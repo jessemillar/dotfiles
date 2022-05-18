6b1691f80a0bc9e2b8a3ad74f1196c5987532194
Jesse Millar <jesse.millar@microsoft.com>
2022-05-18 11:20:04 -0500

    Gotta use sudo

--- bootstrap-reverb.sh
+++ bootstrap-reverb.sh
@@ -3,4 +3,4 @@
 rm reverb-linux-amd64 || true
 curl -OL https://github.com/jessemillar/reverb/releases/latest/download/reverb-linux-amd64
 chmod +x reverb-linux-amd64
-mv reverb-linux-amd64 /usr/local/bin/reverb
+sudo mv reverb-linux-amd64 /usr/local/bin/reverb
