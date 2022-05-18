c8d2cedf0c99bb442f2f7e5f26f562c1756ac251
Jesse Millar <jesse.millar@microsoft.com>
2022-05-18 11:23:08 -0500

    mv to cp

--- bootstrap-reverb.sh
+++ bootstrap-reverb.sh
@@ -3,4 +3,5 @@
 rm reverb-linux-amd64 || true
 curl -OL https://github.com/jessemillar/reverb/releases/latest/download/reverb-linux-amd64
 chmod +x reverb-linux-amd64
-sudo mv reverb-linux-amd64 /usr/local/bin/reverb
+# Copy instead of move since building the Docker container depends on the binary still existing in this spot
+sudo cp reverb-linux-amd64 /usr/local/bin/reverb
