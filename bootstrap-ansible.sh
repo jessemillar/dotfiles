64045b174719ffbc68483c97c2567e529792f23b
Jesse Millar <jesse.millar@microsoft.com>
2023-03-14 15:18:52 +0000

    Fixing an Ansible issue

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -3,5 +3,11 @@
 # Install Ansible via Homebrew
 reverb "Installing Ansible dependencies via apt-get"
 sudo apt-get install -y python3
+
+# prevent ERROR: Ansible could not initialize the preferred locale: unsupported locale setting
+sudo locale-gen en_US.UTF-8
+sudo update-locale LC_ALL="en_US.UTF-8"
+echo "If you get an error about a 'unsupported locale setting' from Ansible, reboot and re-run ./bootstrap.sh"
+
 reverb "Installing Ansible with brew"
 brew install ansible
